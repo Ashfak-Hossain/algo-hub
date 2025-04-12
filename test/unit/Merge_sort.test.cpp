6d6216be2c792aeaa2c63ec6932d21b812fe52ba
Berlin <evan1234.ek@gmail.com>
2025-04-12 11:38:06 +0600

    Refactor Quick and Merge Sort test files: replace std::vector with using directive for cleaner code

--- test/unit/Merge_sort.test.cpp
+++ test/unit/Merge_sort.test.cpp
@@ -7,6 +7,9 @@
 #include <algorithm>
 #include <random>
 
+using std::vector;
+using std::sort;
+
 class TestMergeSort : public BerlinTestBase {
 public:
   TestMergeSort() : BerlinTestBase("TestMergeSort") {}
@@ -14,36 +17,36 @@ public:
   void run(int id) override {
     switch (id) {
       case 0: {
-        std::vector<int> arr = {5, 3, 8, 6, 2};
-        std::vector<int> expected = {2, 3, 5, 6, 8};
+        vector<int> arr = {5, 3, 8, 6, 2};
+        vector<int> expected = {2, 3, 5, 6, 8};
         merge_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 1: {
-        std::vector<int> arr = {1};
-        std::vector<int> expected = {1};
+        vector<int> arr = {1};
+        vector<int> expected = {1};
         merge_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 2: {
-        std::vector<int> arr = {10, 10, 10};
-        std::vector<int> expected = {10, 10, 10};
+        vector<int> arr = {10, 10, 10};
+        vector<int> expected = {10, 10, 10};
         merge_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 3: {
-        std::vector<int> arr = {-3, -1, -2, 0, 2};
-        std::vector<int> expected = {-3, -2, -1, 0, 2};
+        vector<int> arr = {-3, -1, -2, 0, 2};
+        vector<int> expected = {-3, -2, -1, 0, 2};
         merge_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 4: {
-        std::vector<int> arr = {};
-        std::vector<int> expected = {};
+        vector<int> arr = {};
+        vector<int> expected = {};
         merge_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
@@ -51,7 +54,7 @@ public:
       case 5: {
         auto arr = berlin_rand::randvec<int>(100, -1000, 1000);
         auto expected = arr;
-        std::sort(expected.begin(), expected.end());
+        sort(expected.begin(), expected.end());
         merge_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
--- test/unit/Quick_sort.test.cpp
+++ test/unit/Quick_sort.test.cpp
@@ -6,6 +6,9 @@
 #include <vector>
 #include <algorithm>
 
+using std::vector;
+using std::sort;
+
 class TestQuickSort : public BerlinTestBase {
 public:
   TestQuickSort() : BerlinTestBase("TestQuickSort") {}
@@ -13,36 +16,36 @@ public:
   void run(int id) override {
     switch (id) {
       case 0: {
-        std::vector<int> arr = {5, 3, 8, 6, 2};
-        std::vector<int> expected = {2, 3, 5, 6, 8};
+        vector<int> arr = {5, 3, 8, 6, 2};
+        vector<int> expected = {2, 3, 5, 6, 8};
         quick_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 1: {
-        std::vector<int> arr = {1};
-        std::vector<int> expected = {1};
+        vector<int> arr = {1};
+        vector<int> expected = {1};
         quick_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 2: {
-        std::vector<int> arr = {10, 10, 10};
-        std::vector<int> expected = {10, 10, 10};
+        vector<int> arr = {10, 10, 10};
+        vector<int> expected = {10, 10, 10};
         quick_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 3: {
-        std::vector<int> arr = {-3, -1, -2, 0, 2};
-        std::vector<int> expected = {-3, -2, -1, 0, 2};
+        vector<int> arr = {-3, -1, -2, 0, 2};
+        vector<int> expected = {-3, -2, -1, 0, 2};
         quick_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
       }
       case 4: {
-        std::vector<int> arr = {};
-        std::vector<int> expected = {};
+        vector<int> arr = {};
+        vector<int> expected = {};
         quick_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
@@ -50,7 +53,7 @@ public:
       case 5: { 
         auto arr = berlin_rand::randvec<int>(100, -1000, 1000); 
         auto expected = arr;
-        std::sort(expected.begin(), expected.end());
+        sort(expected.begin(), expected.end());
         quick_sort(arr.begin(), arr.end());
         check(arr, expected);
         break;
