930e769f7921aa954e114228ff5462dde0c752ed
Berlin <evan1234.ek@gmail.com>
2025-04-13 15:01:05 +0600

    Refactor: Simplify BERLIN_REGISTER_TEST macro definition

--- test/core/BerlinTestRegister.h
+++ test/core/BerlinTestRegister.h
@@ -2,6 +2,4 @@
 
 #include "BerlinTestWrapper.h"
 
-// Macro to register a test case automatically at startup
-#define BERLIN_REGISTER_TEST(TestClass) \
-  BerlinTestWrapper* _berlin_test_##TestClass = new BerlinTestWrapper(new TestClass());
+#define BERLIN_REGISTER_TEST(TestClass) BerlinTestWrapper* _berlin_test_##TestClass = new BerlinTestWrapper(new TestClass());
